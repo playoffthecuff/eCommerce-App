13bcfa202f3e2ee3cf461a54cee7b6389567c7c0
playoffthecuff <jmobile@bk.ru>
2024-05-01 19:25:13 +0300

    chore: RSS-ECOMM-1_08 Configure bundler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+type ViteConfigOutput = {
+  mode: string;
+  command: string;
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default (args: ViteConfigOutput) => {
+
+  const generateScopedName = args.mode === 'production' ? '[hash:base64:3]' : '[local]_[hash:base64:3]';
+
+  return defineConfig({
+    plugins: [react()],
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName,
+      },
+    },
+  })
+}
