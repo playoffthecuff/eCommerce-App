e4395e56a8ced3ee5f5c00dabe112054dd156b27
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-02 23:19:21 +0300

    chore: install vitest, change vite.config.ts accordingly, add App.tsx test

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Renders main page correctly', async () => {
+  it('Should render the page correctly', async () => {
+    expect(true).toBeTruthy();
+  });
+});
