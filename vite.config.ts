8b0b610364437d4ff6a71757fb966bbe8be0546b
Katsiaryna Rubchanka <6923845@mail.ru>
2024-05-02 21:56:31 +0300

    chore: add husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 type ViteConfigOutput = {
   mode: string;
   command: string;
-}
+};
 
 // https://vitejs.dev/config/
 export default (args: ViteConfigOutput) => {
-
   const generateScopedName = args.mode === 'production' ? '[hash:base64:3]' : '[local]_[hash:base64:3]';
 
   return defineConfig({
