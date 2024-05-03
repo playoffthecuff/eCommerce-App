46c9c4f30ec8b31e08e9409877afe8aa667e90a0
playoffthecuff <jmobile@bk.ru>
2024-05-03 12:20:01 +0300

    git Revert "chore: RSS-ECOMM-1_08 Configure bundler (#10)"
    This reverts commit 1c84900618b79ffc963d66f75cced1a022e289e3.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
 import './App.css';
-import styles from './test.module.css';
-
-function App() {
-
-  return (
-    <>
-      <h1 className={styles.testOk}>Hello Team!</h1>
-    </>
-  )
 
 function App() {
   return <h1>Hello Team!</h1>;
