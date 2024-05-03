1c84900618b79ffc963d66f75cced1a022e289e3
playoffthecuff <149590154+playoffthecuff@users.noreply.github.com>
2024-05-03 12:07:58 +0300

    chore: RSS-ECOMM-1_08 Configure bundler (#10)
    * chore: RSS-ECOMM-1_08 Configure bundler
    
    * chore: RSS-ECOMM-1_08 Configure bundler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import './App.css';
+import styles from './test.module.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className={styles.testOk}>Hello Team!</h1>
+    </>
+  )
 
 function App() {
   return <h1>Hello Team!</h1>;
