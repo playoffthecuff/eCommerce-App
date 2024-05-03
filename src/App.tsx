6809d9fc42d3b57ac74ff0328093d6bb0303d035
playoffthecuff <jmobile@bk.ru>
2024-05-03 12:31:34 +0300

    chore: RSS-ECOMM-1_08 Configure bundler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
+import styles from './test.module.css';
 
 function App() {
-  return <h1>Hello Team!</h1>;
+  return <h1 className={styles.testOk}>Hello Team!</h1>;
 }
 
 export default App;
