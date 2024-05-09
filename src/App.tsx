548b97fe3e5be64516d38e8cd071739a6dbd4b7f
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-09 20:27:07 +0300

    fix: move ant App inside Config Provider
    Since styles don't apply to the app, nested App component inside Config Provider

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,37 @@ const router = createHashRouter(routerConfig);
 
 function AppWrapper() {
   return (
-    <App className="app">
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: '#cd4c1d',
-            colorPrimaryHover: '#111',
-            borderRadius: 0,
-            colorLink: '#cd4c1d',
-            fontFamily: 'Haas',
-            colorError: '#9f2d11',
-            linkDecoration: 'underline',
-            linkHoverDecoration: 'underline',
-            colorLinkHover: '#9f2d11',
-            controlHeight: 36,
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#cd4c1d',
+          colorPrimaryHover: '#111',
+          borderRadius: 0,
+          colorLink: '#cd4c1d',
+          fontFamily: 'Haas',
+          colorError: '#9f2d11',
+          linkDecoration: 'underline',
+          linkHoverDecoration: 'underline',
+          colorLinkHover: '#9f2d11',
+          controlHeight: 36,
+        },
+        components: {
+          Button: {
+            fontFamily: 'Futura',
+            fontWeight: 'bold',
+            primaryShadow: 'none',
           },
-          components: {
-            Button: {
-              fontFamily: 'Futura',
-              fontWeight: 'bold',
-              primaryShadow: 'none',
-            },
-            Form: {
-              itemMarginBottom: 28,
-              verticalLabelPadding: 0,
-            },
+          Form: {
+            itemMarginBottom: 28,
+            verticalLabelPadding: 0,
           },
-        }}
-      />
-      <RouterProvider router={router} />
-    </App>
+        },
+      }}
+    >
+      <App className="app">
+        <RouterProvider router={router} />
+      </App>
+    </ConfigProvider>
   );
 }
 
