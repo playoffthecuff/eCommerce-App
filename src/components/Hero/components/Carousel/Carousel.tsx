94eb137ca2192fedaf8651b49299dd08bdb53631
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-17 21:57:29 +0300

    feat: add content to the slides

--- src/components/Hero/components/Carousel/Carousel.tsx
+++ src/components/Hero/components/Carousel/Carousel.tsx
@@ -1,29 +1,51 @@
-import { Carousel as AntCarousel } from 'antd';
+import { Carousel as AntCarousel, Typography } from 'antd';
 
 import styles from './Carousel.module.css';
 
-const contentStyle: React.CSSProperties = {
-  height: '100%',
-  color: '#fff',
-  textAlign: 'center',
-  // lineHeight: '80vh',
-  // background: '#364d79',
-};
+// const contentStyle: React.CSSProperties = {
+//   height: '100%',
+//   color: '#fff',
+//   textAlign: 'center',
+//   // lineHeight: '80vh',
+//   // background: '#364d79',
+// };
+
+const { Title, Paragraph } = Typography;
 
 function Carousel() {
   return (
-    <AntCarousel className={styles.carousel}>
+    <AntCarousel autoplay className={styles.carousel}>
       <div className={`${styles.slide} ${styles['slide-1']}`}>
-        <h3 style={contentStyle}>1</h3>
+        <div className={styles.overlay}>
+          <div className={styles['slide-content']}>
+            <Title className={styles['hero-title']}>Pure Cycles</Title>
+            <Paragraph className={styles['hero-description']}>Best Cycles Store Ever</Paragraph>
+          </div>
+        </div>
       </div>
       <div className={`${styles.slide} ${styles['slide-2']}`}>
-        <h3 style={contentStyle}>2</h3>
+        <div className={styles.overlay}>
+          <div className={styles['slide-content']}>
+            <Title className={styles['hero-title']}>Pure Cycles</Title>
+            <Paragraph className={styles['hero-description']}>Best Cycles Store Ever</Paragraph>
+          </div>
+        </div>
       </div>
       <div className={`${styles.slide} ${styles['slide-3']}`}>
-        <h3 style={contentStyle}>3</h3>
+        <div className={styles.overlay}>
+          <div className={styles['slide-content']}>
+            <Title className={styles['hero-title']}>Pure Cycles</Title>
+            <Paragraph className={styles['hero-description']}>Best Cycles Store Ever</Paragraph>
+          </div>
+        </div>
       </div>
       <div className={`${styles.slide} ${styles['slide-4']}`}>
-        <h3 style={contentStyle}>4</h3>
+        <div className={styles.overlay}>
+          <div className={styles['slide-content']}>
+            <Title className={styles['hero-title']}>Pure Cycles</Title>
+            <Paragraph className={styles['hero-description']}>Best Cycles Store Ever</Paragraph>
+          </div>
+        </div>
       </div>
     </AntCarousel>
   );
