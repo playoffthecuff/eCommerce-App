0c3617bbd1d0479ee7b5d275c9ec9525161802de
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-18 14:59:21 -0500

    style: add adaptive, add image to the error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,7 @@
-import { Space, Typography } from 'antd';
+import { Typography } from 'antd';
 import Paragraph from 'antd/es/typography/Paragraph';
+
+import pageLogo from '../../assets/images/monkeys.svg';
 
 import styles from './ErrorPage.module.css';
 
@@ -11,12 +13,17 @@ function ErrorPage() {
       <div id="error-page" className={styles.error}>
         <Title>Not Found</Title>
         <Paragraph>
-          <Space>
-            <Text>There is plenty to buy on our Website, but nothing here. Head to our</Text>
-            <Link href="#/main">[main page]</Link>
-            <Text>and begin your shopping!</Text>
-          </Space>
+          <Text className={styles.text}>
+            There is plenty to buy on our Website, but nothing here. Head to our{' '}
+            <Link href="#/main" className={styles.text}>
+              [main page]
+            </Link>{' '}
+            and begin your shopping!
+          </Text>
         </Paragraph>
+        <div className={styles['error-image-wrapper']}>
+          <img src={pageLogo} alt="Monkeys Icon" />
+        </div>
       </div>
     </div>
   );
