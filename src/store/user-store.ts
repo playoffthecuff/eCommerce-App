156d2d55fae8f26fa6bb809442d97aaf6fb16d01
playoffthecuff <jmobile@bk.ru>
2024-05-18 20:22:15 +0300

    feat: RSS-ECOMM-2_03 Integrate login form with backend authorization service

--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -1,135 +1,102 @@
 import { action, makeObservable, observable, runInAction } from 'mobx';
-import axios, { AxiosError } from 'axios';
-import { AuthorizationResponse, User } from '../types/authorization-response';
+import { User } from '../types/authorization-response';
 import UserService from '../utils/user-service';
 import { BootState } from '../types/boot-state';
 
 class UserStore {
   private _user: User = { id: null, email: null, isActivated: false };
 
+  public get user(): User {
+    return { ...this._user };
+  }
+
   private _isAuthorized: boolean = false;
+
+  public get isAuthorized(): boolean {
+    return this._isAuthorized;
+  }
 
   private _bootState: BootState = BootState.None;
 
+  get bootState(): BootState {
+    return this._bootState;
+  }
+
   constructor() {
-    makeObservable<UserStore, '_user' | '_isAuthorized' | '_bootState' | 'checkAuthorization'>(this, {
+    makeObservable<UserStore, '_user' | '_isAuthorized' | '_bootState'>(this, {
       _user: observable,
       _isAuthorized: observable,
       _bootState: observable,
       checkAuthorization: action,
+      login: action,
+      registration: action,
+      logout: action,
     });
     this.checkAuthorization();
   }
 
-  public async login(email: string, password: string) {
-    try {
-      const response = await UserService.login(email, password);
-      console.log(response);
-      localStorage.setItem('token', response.data.accessToken);
-      runInAction(() => {
-        this._isAuthorized = true;
-        this._user = response.data.user;
-      });
-    } catch (err) {
-      const error = err as AxiosError | Error;
-      let message;
-      if (axios.isAxiosError(error)) {
-        const responseData: { message?: string } = error.response?.data ?? {};
-        console.log(error.message);
-        message = responseData.message ?? 'connection failed';
-      } else {
-        message = 'unexplained error';
-      }
-      console.log(message);
-      throw Error(message);
-    }
-  }
-
-  public async registration(email: string, password: string) {
-    try {
-      const response = await UserService.registration(email, password);
-      console.log(response);
-      localStorage.setItem('token', response.data.accessToken);
-      runInAction(() => {
-        this._isAuthorized = true;
-        this._user = response.data.user;
-      });
-    } catch (err) {
-      const error = err as AxiosError | Error;
-      let message;
-      if (axios.isAxiosError(error)) {
-        const responseData: { message?: string } = error.response?.data ?? {};
-        console.log(error.message);
-        message = responseData.message ?? 'connection failed';
-      } else {
-        message = 'unexplained error';
-      }
-      console.log(message);
-    }
-  }
-
-  public async logout() {
-    try {
-      await UserService.logout();
-      localStorage.removeItem('token');
-      runInAction(() => {
-        this._isAuthorized = false;
-        this._user = { id: null, email: null, isActivated: false };
-      });
-    } catch (err) {
-      const error = err as AxiosError | Error;
-      let message;
-      if (axios.isAxiosError(error)) {
-        const responseData: { message?: string } = error.response?.data ?? {};
-        console.log(error.message);
-        message = responseData.message ?? 'connection failed';
-      } else {
-        message = 'unexplained error';
-      }
-      console.log(message);
-    }
-  }
-
-  public async checkAuthorization() {
+  public async login(email: string, password: string): Promise<void> {
     this._bootState = BootState.InProgress;
-    try {
-      const response = await axios.get<AuthorizationResponse>(`${import.meta.env.VITE_API_URL}/users/refresh`, {
-        withCredentials: true,
-      });
-      console.log(response);
-      localStorage.setItem('token', response.data.accessToken);
+    const [response, errorMessage] = await UserService.login(email, password);
+    if (errorMessage) {
+      this._bootState = BootState.Failed;
+      throw Error(errorMessage);
+    }
+    if (response) {
       runInAction(() => {
         this._isAuthorized = true;
         this._user = response.data.user;
-      });
-    } catch (err) {
-      const error = err as AxiosError | Error;
-      let message;
-      if (axios.isAxiosError(error)) {
-        const responseData: { message?: string } = error.response?.data ?? {};
-        console.log(error.message);
-        message = responseData.message ?? 'connection failed';
-      } else {
-        message = 'unexplained error';
-      }
-      console.log(message);
-    } finally {
-      runInAction(() => {
         this._bootState = BootState.Success;
       });
     }
   }
 
-  get user(): User {
-    return { ...this._user };
+  public async registration(email: string, password: string): Promise<void> {
+    this._bootState = BootState.InProgress;
+    const [response, errorMessage] = await UserService.registration(email, password);
+    if (errorMessage) {
+      this._bootState = BootState.Failed;
+      throw Error(errorMessage);
+    }
+    if (response) {
+      runInAction(() => {
+        this._isAuthorized = true;
+        this._user = response.data.user;
+        this._bootState = BootState.Success;
+      });
+    }
   }
 
-  get isAuthorized() {
-    return this._isAuthorized;
+  public async logout(): Promise<void> {
+    this._bootState = BootState.InProgress;
+    const [response, errorMessage] = await UserService.logout();
+    if (errorMessage) {
+      this._bootState = BootState.Failed;
+      return;
+    }
+    if (response) {
+      runInAction(() => {
+        this._isAuthorized = false;
+        this._user = { id: null, email: null, isActivated: false };
+        this._bootState = BootState.Success;
+      });
+    }
   }
 
-  get bootState() {
-    return this._bootState;
+  public async checkAuthorization(): Promise<void> {
+    this._bootState = BootState.InProgress;
+    const [response, errorMessage] = await UserService.checkAuthorization();
+    if (errorMessage) {
+      this._bootState = BootState.Failed;
+      return;
+    }
+    if (response) {
+      runInAction(() => {
+        this._isAuthorized = true;
+        this._user = response.data.user;
+        this._bootState = BootState.Success;
+      });
+    }
   }
 }
 
--- src/utils/user-service.ts
+++ src/utils/user-service.ts
@@ -1,18 +1,94 @@
-import { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import api from './api';
 import { AuthorizationResponse } from '../types/authorization-response';
 
 const UserService = {
-  async login(email: string, password: string): Promise<AxiosResponse<AuthorizationResponse>> {
-    return api.post<AuthorizationResponse>('/users/login', { email, password });
+  async login(email: string, password: string): Promise<[AxiosResponse<AuthorizationResponse> | null, string]> {
+    let response: AxiosResponse<AuthorizationResponse> | null = null;
+    let error: Error | AxiosError | null = null;
+    let message: string = '';
+    try {
+      response = await api.post<AuthorizationResponse>('/users/login', { email, password });
+      console.log(response);
+      localStorage.setItem('token', response.data.accessToken);
+    } catch (err) {
+      error = err as AxiosError | Error;
+      if (axios.isAxiosError(error)) {
+        const responseData: { message?: string } = error.response?.data ?? {};
+        console.log(error.message);
+        message = responseData.message ?? 'connection failed';
+      } else {
+        message = 'unexplained error';
+      }
+      console.log(message);
+    }
+    return [response, message];
   },
 
-  async registration(email: string, password: string): Promise<AxiosResponse<AuthorizationResponse>> {
-    return api.post<AuthorizationResponse>('/users/registration', { email, password });
+  async registration(email: string, password: string): Promise<[AxiosResponse<AuthorizationResponse> | null, string]> {
+    let response: AxiosResponse<AuthorizationResponse> | null = null;
+    let error: Error | AxiosError | null = null;
+    let message: string = '';
+    try {
+      response = await api.post<AuthorizationResponse>('/users/registration', { email, password });
+      console.log(response);
+      localStorage.setItem('token', response.data.accessToken);
+    } catch (err) {
+      error = err as AxiosError | Error;
+      if (axios.isAxiosError(error)) {
+        const responseData: { message?: string } = error.response?.data ?? {};
+        console.log(error.message);
+        message = responseData.message ?? 'connection failed';
+      } else {
+        message = 'unexplained error';
+      }
+      console.log(message);
+    }
+    return [response, message];
   },
 
-  async logout(): Promise<AxiosResponse<AuthorizationResponse>> {
-    return api.post('/users/logout');
+  async logout(): Promise<[AxiosResponse<AuthorizationResponse> | null, string]> {
+    let response: AxiosResponse<AuthorizationResponse> | null = null;
+    let error: Error | AxiosError | null = null;
+    let message: string = '';
+    try {
+      response = await api.post('/users/logout');
+      localStorage.removeItem('token');
+    } catch (err) {
+      error = err as AxiosError | Error;
+      if (axios.isAxiosError(error)) {
+        const responseData: { message?: string } = error.response?.data ?? {};
+        console.log(error.message);
+        message = responseData.message ?? 'connection failed';
+      } else {
+        message = 'unexplained error';
+      }
+      console.log(message);
+    }
+    return [response, message];
+  },
+
+  async checkAuthorization(): Promise<[AxiosResponse<AuthorizationResponse> | null, string]> {
+    let response: AxiosResponse<AuthorizationResponse> | null = null;
+    let error: Error | AxiosError | null = null;
+    let message: string = '';
+    try {
+      response = await axios.get<AuthorizationResponse>(`${import.meta.env.VITE_API_URL}/users/refresh`, {
+        withCredentials: true,
+      });
+      localStorage.setItem('token', response.data.accessToken);
+    } catch (err) {
+      error = err as AxiosError | Error;
+      if (axios.isAxiosError(error)) {
+        const responseData: { message?: string } = error.response?.data ?? {};
+        console.log(error.message);
+        message = responseData.message ?? 'connection failed';
+      } else {
+        message = 'unexplained error';
+      }
+      console.log(message);
+    }
+    return [response, message];
   },
 };
 
