f60c9b2f8934b223baab7e02365142d23171fbc2
playoffthecuff <jmobile@bk.ru>
2024-05-20 01:56:37 +0300

    feat: add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,141 @@
-import { Typography, Space, Button } from 'antd';
-import { FormOutlined } from '@ant-design/icons';
-
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { Typography, Layout, Menu, MenuProps, Switch } from 'antd';
+import {
+  FormOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+  TeamOutlined,
+  WalletOutlined,
+} from '@ant-design/icons';
 import { observer } from 'mobx-react-lite';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Sider from 'antd/es/layout/Sider';
+import { LogoIcon } from '../CustomIcons/CustomIcons';
+
 import styles from './Header.module.css';
 import userStore from '../../store/user-store';
-import LogoutButton from './LogoutButton';
-import LoginButton from './LoginButton';
 
-const { Title, Paragraph } = Typography;
+const { Title, Link } = Typography;
+const { Header: AntHeader } = Layout;
+type MenuItem = Required<MenuProps>['items'][number];
 
 function Header() {
+  const [current, setCurrent] = useState('');
+  const [siderState, setSiderState] = useState(styles.sider);
+  const [burgerState, setBurgerState] = useState(styles['burger-button']);
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+      setSiderState(`${styles.sider} ${styles.active}`);
+      setBurgerState(`${styles['burger-button']} ${styles.active}`);
+    } else {
+      document.body.classList.remove('no-scroll');
+      setSiderState(styles.sider);
+      setBurgerState(styles['burger-button']);
+    }
+  }, [isOpen]);
+  const navigate = useNavigate();
+
+  const menuItems: MenuItem[] = [
+    { label: 'SHOP', key: 'SHOP', icon: <WalletOutlined /> },
+    userStore.isAuthorized
+      ? {
+          label: 'LOG OUT',
+          key: 'LOG OUT',
+          icon: <LogoutOutlined />,
+          onClick: () => {
+            userStore.logout();
+          },
+        }
+      : {
+          label: 'SIGN IN',
+          key: 'SIGN IN',
+          icon: <LoginOutlined />,
+          onClick: () => {
+            navigate('/login');
+          },
+        },
+    userStore.isAuthorized
+      ? {
+          label: 'PROFILE',
+          key: 'PROFILE',
+          icon: <UserOutlined />,
+          // onClick: () => {
+          //   navigate('/profile');
+          // },
+        }
+      : {
+          label: 'SIGN UP',
+          key: 'SIGN UP',
+          icon: <FormOutlined />,
+          onClick: () => {
+            navigate('/registration');
+          },
+        },
+    { label: 'ABOUT US', key: 'ABOUT US', icon: <TeamOutlined /> },
+  ];
+
+  const menuClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key);
+    setIsOpen(false);
+  };
+
+  const burgerClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const changeTheme = (value: boolean) => {
+    return value;
+  };
+
   return (
-    <header className={styles.header}>
-      <Title>Cycling Dependency</Title>
-      <Paragraph>{userStore.isAuthorized ? 'User is authorized' : 'Please sign in or sign up'}</Paragraph>
-      <div className={styles['link-wrapper']}>
-        <Space>
-          <Button type="link" href="#/registration" icon={<FormOutlined />}>
-            Sign Up
-          </Button>
-        </Space>
-        <Space>{userStore.isAuthorized ? <LogoutButton /> : <LoginButton />}</Space>
-      </div>
-    </header>
+    <>
+      <AntHeader className={styles.header}>
+        <div className="layout-container">
+          <Link href="#/">
+            <div className={styles.logo}>
+              <LogoIcon />
+              <Title level={5}>Cycling Dependency</Title>
+            </div>
+          </Link>
+          <div className={styles['burger-wrapper']} onClick={burgerClick}>
+            <div className={styles['burger-button-wrapper']}>
+              <div className={burgerState} />
+            </div>
+          </div>
+          <Menu
+            className={styles['burger-menu']}
+            style={{ lineHeight: '2rem' }}
+            mode="horizontal"
+            items={menuItems}
+            selectedKeys={[current]}
+            onClick={menuClick}
+          />
+          <Menu
+            className={styles.menu}
+            style={{ lineHeight: '2rem' }}
+            mode="horizontal"
+            items={[
+              {
+                key: 'Cart',
+                icon: <ShoppingCartOutlined style={{ fontSize: '24px' }} />,
+              },
+            ]}
+            selectedKeys={[current]}
+            onClick={menuClick}
+          />
+          <Switch onChange={changeTheme} checkedChildren="Dark" unCheckedChildren="Light" />
+        </div>
+      </AntHeader>
+      <Sider width="100%" className={siderState}>
+        <Menu mode="inline" selectedKeys={[current]} onClick={menuClick} items={menuItems} />
+      </Sider>
+    </>
   );
 }
 
--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -12,12 +12,14 @@ const lightTheme: ThemeConfig = {
     colorPrimary: '#2644ad', // cd4c1d
     colorPrimaryHover: '#111',
     colorError: '#ad4426', // cd4c1d
-    colorLink: '#112d9f',
-    colorLinkHover: '#1d4ccd',
     colorSuccess: '#44ad26',
+    colorLink: '#2644ad',
+    colorLinkHover: '#5875da',
     linkDecoration: 'underline',
     linkHoverDecoration: 'underline',
     colorText: '#111',
+    colorBgContainerDisabled: '#d3d3d3',
+    colorTextDisabled: '#a9a9a9',
   },
   components: {
     Button: {
@@ -28,6 +30,17 @@ const lightTheme: ThemeConfig = {
     Form: {
       itemMarginBottom: 28,
       verticalLabelPadding: 0,
+    },
+    Menu: {
+      itemColor: '#111',
+      itemHoverColor: '#2644ad',
+      itemPaddingInline: 12,
+      horizontalItemSelectedColor: '#2644ad',
+      fontFamily: 'Futura',
+    },
+    Layout: {
+      headerBg: '#fff',
+      headerHeight: 48,
     },
   },
 };
