6bc5c5124646fe8e10dd0a5cb0d1f5e6ca7adfd7
Hrybach Oleksiy <84387553+hrybach-oleksiy@users.noreply.github.com>
2024-05-22 11:27:41 -0500

    refactor: change main layout styles (#44)
    * refactor: change main layout styles
    
    * refactor: move wrapper to the general styles
    
    * refactor: refactor styles.css

--- src/components/Hero/subComponents/Carousel/Carousel.tsx
+++ src/components/Hero/subComponents/Carousel/Carousel.tsx
@@ -13,31 +13,28 @@ function Carousel() {
         <div className={`${styles.slide} ${styles['slide-4']}`} />
       </AntCarousel>
       <div className={styles.overlay}>
-        <div className="container">
-          <div className={styles['slide-content']}>
-            <Title className={styles['hero-title']}>Dear Reviewers!</Title>
-            <Paragraph className={styles['hero-description']}>
-              Please note, this is the Full Stack project and here we use our own Backend and do not use Commercetools
-              API
-            </Paragraph>
-            <div className={styles['buttons-wrapper']}>
-              <Button type="primary" href="#/registration" htmlType="button" block className={styles['hero-button']}>
-                Registration Page
-              </Button>
-              <Button type="primary" href="#/login" htmlType="button" block className={styles['hero-button']}>
-                Login Page
-              </Button>
-              <Button
-                type="primary"
-                href="https://codefrondlers.store/jsfe23q4/api-docs/"
-                target="a_blank"
-                htmlType="button"
-                block
-                className={styles['hero-button']}
-              >
-                Api Documentation
-              </Button>
-            </div>
+        <div className={styles['slide-content']}>
+          <Title className={styles['hero-title']}>Dear Reviewers!</Title>
+          <Paragraph className={styles['hero-description']}>
+            Please note, this is the Full Stack project and here we use our own Backend and do not use Commercetools API
+          </Paragraph>
+          <div className={styles['buttons-wrapper']}>
+            <Button type="primary" href="#/registration" htmlType="button" block className={styles['hero-button']}>
+              Registration Page
+            </Button>
+            <Button type="primary" href="#/login" htmlType="button" block className={styles['hero-button']}>
+              Login Page
+            </Button>
+            <Button
+              type="primary"
+              href="https://codefrondlers.store/jsfe23q4/api-docs/"
+              target="a_blank"
+              htmlType="button"
+              block
+              className={styles['hero-button']}
+            >
+              Api Documentation
+            </Button>
           </div>
         </div>
       </div>
