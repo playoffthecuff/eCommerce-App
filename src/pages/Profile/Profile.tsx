70cfa23351e401564944231eb8ec6662ddef1376
Katsiaryna Rubchanka <6923845@mail.ru>
2024-05-23 19:26:16 +0300

    feat: add steps in profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,30 +1,52 @@
-import { Typography } from 'antd';
-import Paragraph from 'antd/es/typography/Paragraph';
-
-import pageLogo from '../../assets/images/monkeys.svg';
-
+import { Form, Input, Steps, Typography } from 'antd';
+import { MailOutlined, SolutionOutlined, UnlockOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 import styles from './Profile.module.css';
 
-const { Title, Text, Link } = Typography;
-
 export default function ProfilePage() {
+  const [current, setCurrent] = useState(0);
   return (
-    <div className="container container-inner">
-      <div id="profile-page" className="wrapper">
-        <Title>Page under construction</Title>
-        <Paragraph>
-          <Text className={styles.text}>
-            Sorry! Head to our
-            <Link href="#/main" className={styles.text}>
-              [main page]
-            </Link>
-            and begin your shopping!
-          </Text>
-        </Paragraph>
-        <div className={styles['error-image-wrapper']}>
-          <img src={pageLogo} alt="Monkeys Icon" />
-        </div>
+    <div className="container">
+      <div id="profile-page" className={styles.wrapper}>
+        <Steps onChange={setCurrent} current={current}>
+          <Steps.Step title="Pesonal" icon={<SolutionOutlined />} />
+          <Steps.Step title="Password" icon={<UnlockOutlined />} />
+          <Steps.Step title="Addresses" icon={<MailOutlined />} />
+        </Steps>
+        <PesonalData />
       </div>
     </div>
+  );
+}
+
+function PesonalData() {
+  return (
+    <>
+      <Typography.Title level={3}>PERSONAL DATA</Typography.Title>
+      <Form.Item
+        label="First name"
+        name="firstName"
+        rules={[
+          { required: true, message: 'Please enter your name!' },
+          { min: 1, message: 'Must be at least 1 characters long!' },
+          { pattern: /^[A-Za-z]*$/, message: 'Must contain only English letters!' },
+        ]}
+        hasFeedback
+      >
+        <Input data-testid="firstName" placeholder="Enter your name..." />
+      </Form.Item>
+      <Form.Item
+        label="Last Name"
+        name="lastName"
+        rules={[
+          { required: true, message: 'Please enter your last name!' },
+          { min: 1, message: 'Must be at least 1 characters long!' },
+          { pattern: /^[A-Za-z]*$/, message: 'Must contain only English letters!' },
+        ]}
+        hasFeedback
+      >
+        <Input data-testid="lastName" placeholder="Enter your email last name..." />
+      </Form.Item>
+    </>
   );
 }
