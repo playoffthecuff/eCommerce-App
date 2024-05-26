3daa1813e016c7be4f8be1484dff2cfbd76c2da0
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-26 17:48:29 +0300

    feat: add content to the filter menu

--- src/components/ProductControls/FiltersBlock/FiltersBlock.tsx
+++ src/components/ProductControls/FiltersBlock/FiltersBlock.tsx
@@ -1,14 +1,7 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Typography, Collapse, Checkbox, Slider } from 'antd';
 
 import { useEffect, useState } from 'react';
-import {
-  CloseOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
+import { CloseOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 
 import { observer } from 'mobx-react-lite';
 // import { productsStore } from '../../store/product-store';
@@ -16,28 +9,31 @@ import styles from './FiltersBlock.module.css';
 import CustomButton from '../../CustomButton/CustomButton';
 
 const { Sider } = Layout;
+const { Title } = Typography;
+const { Panel } = Collapse;
+
+const filterData = {
+  'PRODUCT TYPE': [
+    { label: 'Bicycles', count: 6, key: 1 },
+    { label: 'Coaster Bikes', count: 1, key: 2 },
+    { label: 'Pure City Bikes', count: 2, key: 3 },
+    { label: 'Urban Bikes', count: 1, key: 4 },
+  ],
+  BRAND: [{ label: 'Pure Cycles', count: 10, key: 5 }],
+  AVAILABILITY: [
+    { label: 'In stock', count: 10, key: 6 },
+    { label: 'Out of stock', count: 1, key: 7 },
+  ],
+  'POSTAL CODE PATTERN': [
+    { label: '9999', count: 10, key: 7 },
+    { label: '99999', count: 5, key: 8 },
+    { label: '999999', count: 8, key: 9 },
+  ],
+};
 
 export default observer(function FiltersBlock() {
   const [collapsed, setCollapsed] = useState(true);
-
-  const handleMenuClick = ({ key }: { key: string }) => {
-    let category = '';
-    switch (key) {
-      case '1':
-        category = '9999';
-        break;
-      case '2':
-        category = '99999';
-        break;
-      case '3':
-        category = '999999';
-        break;
-      default:
-        category = '';
-    }
-    // productsStore.selectedCategory = category;
-    console.log(category);
-  };
+  const [priceRange, setPriceRange] = useState([0, 1000]);
 
   const toggleMenu = () => {
     setCollapsed(!collapsed);
@@ -49,9 +45,13 @@ export default observer(function FiltersBlock() {
     document.body.style.overflow = 'auto';
   };
 
+  const handlePriceChange = (value: number[]) => {
+    setPriceRange(value);
+  };
+
   useEffect(() => {
     return () => {
-      document.body.style.overflow = 'auto'; // Clean up on component unmount
+      document.body.style.overflow = 'auto';
     };
   }, []);
 
@@ -63,36 +63,48 @@ export default observer(function FiltersBlock() {
         collapsible
         collapsed={collapsed}
         theme="light"
-        width="40%"
+        width="20%"
         style={{ left: collapsed ? '-100%' : '0', position: 'fixed', height: '100vh', top: '48px' }}
       >
-        <div className="demo-logo-vertical" />
-        <div className={styles['close-button']} onClick={closeMenu}>
-          <CloseOutlined />
+        <div className={styles['menu-header']}>
+          <Title level={2} className={styles['menu-title']}>
+            Filter
+          </Title>
+          <CloseOutlined className={styles['close-button']} onClick={closeMenu} />
         </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          onClick={handleMenuClick}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: '9999',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: '99999',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: '999999',
-            },
-          ]}
-        />
+        <Collapse bordered={false} defaultActiveKey={['1']} className={styles['filter-collapse']}>
+          <Panel header="PRICE" key="0" className={styles['filter-panel']}>
+            <div className={styles['filter-item']}>
+              <Slider
+                range
+                min={0}
+                max={1000}
+                defaultValue={[0, 1000]}
+                onChange={handlePriceChange}
+                value={priceRange}
+              />
+              <div className={styles['price-range']}>
+                ${priceRange[0]} - ${priceRange[1]}
+              </div>
+            </div>
+          </Panel>
+          {Object.entries(filterData).map(([category, items], index) => (
+            <Panel header={category} key={index.toString() + 1} className={styles['filter-panel']}>
+              {items.map((item) => (
+                <div key={item.key} className={styles['filter-item']}>
+                  <Checkbox>
+                    {item.label} ({item.count})
+                  </Checkbox>
+                </div>
+              ))}
+            </Panel>
+          ))}
+        </Collapse>
+        <div className={styles['apply-button-wrapper']}>
+          <CustomButton style={{ width: '100px' }} variety="common" htmlType="button">
+            Apply
+          </CustomButton>
+        </div>
       </Sider>
 
       <CustomButton
@@ -102,7 +114,7 @@ export default observer(function FiltersBlock() {
         icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
         onClick={toggleMenu}
       >
-        Filters
+        Filter
       </CustomButton>
     </div>
   );
