78010c4c740231a73753da47d0b0a772c24663f2
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-05-31 15:50:51 +0300

    fix: delete unnecessary types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,93 +1,3 @@
-interface Note {
-  Specifications: string;
-}
-
-interface SizingDetails {
-  'Seat Tube': number;
-  'Top Tube Horizontal': number;
-  'Head Tube Angle': number;
-  'Seat Angle': number;
-  Chainstays: number;
-  'BB-Height to Hub': number;
-  'Wheel Base': number;
-  'Head Tube': number;
-  Reach: number;
-  Stack: number;
-  _id: string;
-}
-
-interface Sizing {
-  [key: string]: SizingDetails;
-}
-
-interface Frameset {
-  Frame: string;
-  Fork: string;
-}
-
-interface Drivetrain {
-  'Crank Set': string;
-  Chainwheel: string;
-  'Bottom Bracket': string;
-  Sprocket: string | null;
-  Freewheel: string | null;
-  'Rear Shifter': string;
-  Chain: string;
-  Pedals: string;
-}
-
-interface Brakes {
-  'Front Brake': string;
-  'Rear Brake': string;
-  'Brake Lever': string;
-}
-
-interface Components {
-  Handlebar: string;
-  Stem: string;
-  Headset: string;
-  Grips: string;
-  Saddle: string;
-  'Seat Post': string;
-  'Seat Clamp': string;
-}
-
-interface Wheels {
-  'Front Wheel': string;
-  'Rear Wheel': string;
-  'Front Hub': string;
-  'Rear Hub': string;
-  Spokes: string;
-  Rims: string;
-  Tires: string;
-  Tubes: string;
-}
-
-interface Specs {
-  frameset: Frameset;
-  drivetrain: Drivetrain;
-  brakes: Brakes;
-  components: Components;
-  wheels: Wheels;
-  _id: string;
-}
-
-export interface ProductData {
-  notes?: Note;
-  sizing: Sizing;
-  _id: string;
-  category?: string;
-  rating: number;
-  title: string;
-  description?: string;
-  'vendor code'?: number;
-  price: number;
-  'discounted price'?: number;
-  color?: string;
-  overview?: string[];
-  specs?: Specs;
-  weight?: number;
-}
 export interface ProductSummary {
   price: number;
   rating: number;
@@ -110,11 +20,6 @@ export type FiltersData = {
   minPrice?: number;
   maxPrice?: number;
 } | null;
-
-export interface Filter {
-  key: string;
-  value: string[] | number[];
-}
 
 export interface Sort {
   field: string;
