781c884f2381298a2708fa1b351947513c28589e
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-06-02 00:05:39 +0300

    feat: implement live search

--- src/components/ProductControls/SearchBlock/SearchBlock.tsx
+++ src/components/ProductControls/SearchBlock/SearchBlock.tsx
@@ -1,7 +1,9 @@
 import { Input } from 'antd';
+import { debounce } from 'lodash';
 import type { SearchProps } from 'antd/es/input/Search';
 
 import { observer } from 'mobx-react-lite';
+import { useCallback } from 'react';
 import styles from './SearchBlock.module.css';
 import { productsStore } from '../../../store/catalog-store';
 
@@ -10,13 +12,25 @@ const { Search } = Input;
 export default observer(function SearchBlock() {
   const { payload } = productsStore;
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSearch = useCallback(
+    debounce((value: string) => {
+      productsStore.applyFilters({
+        query: value,
+        filters: payload.filters,
+        page: payload.page,
+        pageSize: payload.pageSize,
+      });
+    }, 750),
+    [payload.filters, payload.page, payload.pageSize]
+  );
+
   const onSearch: SearchProps['onSearch'] = (value: string) => {
-    productsStore.applyFilters({
-      query: value,
-      filters: payload.filters,
-      page: payload.page,
-      pageSize: payload.pageSize,
-    });
+    debouncedSearch(value);
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSearch(event.target.value);
   };
 
   return (
@@ -24,6 +38,7 @@ export default observer(function SearchBlock() {
       placeholder="search bicycle"
       allowClear
       onSearch={onSearch}
+      onChange={onChange}
       style={{ width: 300 }}
       className={styles['search-input']}
     />
