6b8e94527f90d6e15d94a3e4cfd6df741eecca38
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-06-02 13:38:44 +0300

    feat: delete live search
    since we decided to use search by click, deleted the lodash and debounced search

--- src/components/ProductControls/SearchBlock/SearchBlock.tsx
+++ src/components/ProductControls/SearchBlock/SearchBlock.tsx
@@ -1,9 +1,7 @@
 import { Input } from 'antd';
-import { debounce } from 'lodash';
 import type { SearchProps } from 'antd/es/input/Search';
 
 import { observer } from 'mobx-react-lite';
-import { useCallback } from 'react';
 import styles from './SearchBlock.module.css';
 import { catalogStore } from '../../../store/catalog-store';
 
@@ -12,24 +10,12 @@ const { Search } = Input;
 export default observer(function SearchBlock() {
   const { payload, applyFilters } = catalogStore;
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const debouncedSearch = useCallback(
-    debounce((value: string) => {
-      applyFilters({
-        ...payload,
-        query: value,
-        page: 1,
-      });
-    }, 750),
-    [payload.filters, payload.page, payload.pageSize]
-  );
-
   const onSearch: SearchProps['onSearch'] = (value: string) => {
-    debouncedSearch(value);
-  };
-
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    debouncedSearch(event.target.value);
+    applyFilters({
+      ...payload,
+      query: value,
+      page: 1,
+    });
   };
 
   return (
@@ -37,7 +23,6 @@ export default observer(function SearchBlock() {
       placeholder="search bicycle"
       allowClear
       onSearch={onSearch}
-      onChange={onChange}
       style={{ width: 300 }}
       className={styles['search-input']}
     />
