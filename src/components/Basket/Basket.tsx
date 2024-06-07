a9f7e075dff6fd1253598a3fe344412848cc4678
Katsiaryna Rubchanka <6923845@mail.ru>
2024-06-07 13:02:53 +0300

    feat: add basket page

---
+++ src/components/Basket/Basket.tsx
@@ -0,0 +1,3 @@
+export const Basket = () => {
+  return console.log('Basket Page');
+};
---
+++ src/pages/BasketPage/BasketPage.tsx
@@ -0,0 +1,8 @@
+// import { Basket } from '../../components/Basket/Basket';
+
+// function BasketPage() {
+//   return (
+//   );
+// }
+
+// export default BasketPage;
