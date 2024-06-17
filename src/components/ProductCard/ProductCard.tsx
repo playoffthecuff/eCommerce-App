d4f541c3ab11e0aa4c6d194b4db3b302e370d117
Katsiaryna Rubchanka <6923845@mail.ru>
2024-06-17 20:48:05 +0300

    feat: add notification message

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,6 @@
-import { Card, Radio, RadioChangeEvent, Rate, Skeleton, Tooltip } from 'antd';
+import { Card, Radio, RadioChangeEvent, Rate, Skeleton, Tooltip, notification } from 'antd';
 import classNames from 'classnames';
-import { ShoppingFilled, ShoppingTwoTone } from '@ant-design/icons';
+import { FrownOutlined, ShoppingFilled, ShoppingTwoTone, SmileOutlined } from '@ant-design/icons';
 import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -19,6 +19,7 @@ export default observer(function ProductCard({ product, loading }: ProductCardPr
   const { title, price, discountedPrice, vendorCode, rating, thumbs, _id: id } = product;
   const navigate = useNavigate();
   const [sizeValue, setSizeValue] = useState<CartItem['size']>('M');
+  const [notificationAPI, contextHolder] = notification.useNotification();
 
   const onChange = (event: RadioChangeEvent) => {
     setSizeValue(event.target.value);
@@ -27,87 +28,106 @@ export default observer(function ProductCard({ product, loading }: ProductCardPr
   const handleAddToCart = async (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
     event.stopPropagation();
     event.preventDefault();
-    await cartStore.addToCart({
-      productId: id,
-      size: sizeValue,
-      quantity: 1,
-    });
+    try {
+      await cartStore.addToCart({
+        productId: id,
+        size: sizeValue,
+        quantity: 1,
+      });
+      notificationAPI.success({
+        message: `You have added the item to the cart.`,
+        placement: 'top',
+        icon: <SmileOutlined />,
+        duration: 2.5,
+      });
+    } catch (err) {
+      notificationAPI.error({
+        message: `Failed to add the item.`,
+        description: 'Please try again.',
+        placement: 'top',
+        icon: <FrownOutlined />,
+        duration: 2,
+      });
+    }
   };
 
   const itemInCart = Boolean(cartStore.getCartItem(id, sizeValue));
 
   return (
-    <a href={`${import.meta.env.BASE_URL}#/product?vc=${vendorCode}`} className={styles['product-card-link']}>
-      <Card
-        className={styles.productCard}
-        hoverable
-        cover={
-          loading === BootState.InProgress ? (
-            <Skeleton.Image className={styles.skeletonImage} active />
-          ) : (
-            <img
-              alt={title}
-              src={thumbs ? `data:image/jpeg;base64,${thumbs}` : placeholder}
-              className={styles.productImage}
-            />
-          )
-        }
-      >
-        <Skeleton loading={loading === BootState.InProgress} active paragraph={{ rows: 2 }}>
-          <Card.Meta title={title} className={styles['product-title']} />
-          <div className={styles['size-block']}>
-            <span>Size:</span>
-            <Radio.Group className={styles['size-radio-wrapper']} onChange={onChange} value={sizeValue}>
-              <Radio.Button value="S">S</Radio.Button>
-              <Radio.Button value="M">M</Radio.Button>
-              <Radio.Button value="L">L</Radio.Button>
-            </Radio.Group>
-          </div>
-          <div className={styles['card-body-wrapper']}>
-            <div className={styles.productPrice}>
-              {discountedPrice ? (
-                <>
-                  <span className={classNames(styles.originalPrice, styles.lineThrough)}>${price}</span>
-                  <span className={styles.discountedPrice}>${discountedPrice}</span>
-                </>
-              ) : (
-                <span>${price}</span>
-              )}
+    <>
+      <a href={`${import.meta.env.BASE_URL}#/product?vc=${vendorCode}`} className={styles['product-card-link']}>
+        <Card
+          className={styles.productCard}
+          hoverable
+          cover={
+            loading === BootState.InProgress ? (
+              <Skeleton.Image className={styles.skeletonImage} active />
+            ) : (
+              <img
+                alt={title}
+                src={thumbs ? `data:image/jpeg;base64,${thumbs}` : placeholder}
+                className={styles.productImage}
+              />
+            )
+          }
+        >
+          <Skeleton loading={loading === BootState.InProgress} active paragraph={{ rows: 2 }}>
+            <Card.Meta title={title} className={styles['product-title']} />
+            <div className={styles['size-block']}>
+              <span>Size:</span>
+              <Radio.Group className={styles['size-radio-wrapper']} onChange={onChange} value={sizeValue}>
+                <Radio.Button value="S">S</Radio.Button>
+                <Radio.Button value="M">M</Radio.Button>
+                <Radio.Button value="L">L</Radio.Button>
+              </Radio.Group>
             </div>
-            <div>
-              {itemInCart && (
-                <Tooltip title="Go to Cart">
-                  <ShoppingFilled
-                    style={{ fontSize: '24px', color: 'green' }}
-                    onClick={(event) => {
-                      event.stopPropagation();
-                      event.preventDefault();
-                      navigate('/cart');
-                      window.scrollTo({ top: 0, behavior: 'smooth' });
-                    }}
-                  />
-                </Tooltip>
-              )}
-              {!itemInCart && (
-                <Tooltip title="Add to Cart">
-                  <ShoppingTwoTone style={{ fontSize: '24px' }} onClick={handleAddToCart} />
-                </Tooltip>
-              )}
+            <div className={styles['card-body-wrapper']}>
+              <div className={styles.productPrice}>
+                {discountedPrice ? (
+                  <>
+                    <span className={classNames(styles.originalPrice, styles.lineThrough)}>${price}</span>
+                    <span className={styles.discountedPrice}>${discountedPrice}</span>
+                  </>
+                ) : (
+                  <span>${price}</span>
+                )}
+              </div>
+              <div>
+                {itemInCart && (
+                  <Tooltip title="Go to Cart">
+                    <ShoppingFilled
+                      style={{ fontSize: '24px', color: 'green' }}
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        event.preventDefault();
+                        navigate('/cart');
+                        window.scrollTo({ top: 0, behavior: 'smooth' });
+                      }}
+                    />
+                  </Tooltip>
+                )}
+                {!itemInCart && (
+                  <Tooltip title="Add to Cart">
+                    <ShoppingTwoTone style={{ fontSize: '24px' }} onClick={handleAddToCart} />
+                  </Tooltip>
+                )}
+              </div>
             </div>
-          </div>
-          <div className={styles['rate-wrapper']}>
-            <Rate
-              allowHalf
-              value={rating}
-              disabled
-              className={styles.rate}
-              style={{ color: 'var(--color-text)', fontSize: 10 }}
-            />
-            <div className={styles['rate-line']} />
-          </div>
-          <div className={styles['product-card-notification-container']} />
-        </Skeleton>
-      </Card>
-    </a>
+            <div className={styles['rate-wrapper']}>
+              <Rate
+                allowHalf
+                value={rating}
+                disabled
+                className={styles.rate}
+                style={{ color: 'var(--color-text)', fontSize: 10 }}
+              />
+              <div className={styles['rate-line']} />
+            </div>
+            <div className={styles['product-card-notification-container']} />
+          </Skeleton>
+        </Card>
+      </a>
+      {contextHolder}
+    </>
   );
 });
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import classNames from 'classnames';
 
-import { Rate, Typography, InputNumber, Divider, Radio, Spin } from 'antd';
+import { Rate, Typography, InputNumber, Divider, Radio, Spin, Image, Space, notification } from 'antd';
+import { ZoomOutOutlined, ZoomInOutlined, SmileOutlined, FrownOutlined } from '@ant-design/icons';
 import TechSpecs from '../../components/TechSpecs/TechSpecs';
 import Geometry from '../../components/Geometry/Geometry';
 import NoProductResult from '../../components/NoProductResult/NoProductResult';
@@ -28,6 +29,7 @@ function ProductPage() {
   const [quantity, setQuantity] = useState(1);
   const [query] = useSearchParams();
   const vendorCode = query.get('vc')!;
+  const [notificationAPI, contextHolder] = notification.useNotification();
 
   useEffect(() => {
     cartStore.loadItems();
@@ -40,17 +42,49 @@ function ProductPage() {
     fetchData();
   }, [location.search]);
 
-  const handleAddItem = () => {
-    cartStore.addToCart({
-      productId: productStore.product!._id,
-      size,
-      quantity,
-    });
+  const handleAddItem = async () => {
+    try {
+      await cartStore.addToCart({
+        productId: productStore.product!._id,
+        size,
+        quantity,
+      });
+      notificationAPI.success({
+        message: `You have added the item to the cart.`,
+        placement: 'top',
+        icon: <SmileOutlined />,
+        duration: 2.5,
+      });
+    } catch (err) {
+      notificationAPI.error({
+        message: `Failed to add the item.`,
+        description: 'Please try again.',
+        placement: 'top',
+        icon: <FrownOutlined />,
+        duration: 2,
+      });
+    }
   };
 
-  const handleRemoveItem = () => {
+  const handleRemoveItem = async () => {
     setQuantity(1);
-    cartStore.removeFromCart(productStore.product!._id, size);
+    try {
+      await cartStore.removeFromCart(productStore.product!._id, size);
+      notificationAPI.success({
+        message: `You have delete the item.`,
+        placement: 'top',
+        icon: <SmileOutlined />,
+        duration: 2.5,
+      });
+    } catch (err) {
+      notificationAPI.error({
+        message: `Failed to delete the item.`,
+        description: 'Please try again.',
+        placement: 'top',
+        icon: <FrownOutlined />,
+        duration: 2,
+      });
+    }
   };
 
   if (productStore.bootState === BootState.InProgress) {
@@ -62,117 +96,120 @@ function ProductPage() {
   const cartItem = cartStore.getCartItem(productStore.product!._id, size);
 
   return (
-    <div className={styles.container}>
-      <div className={styles['product-container']}>
-        <div className={styles['image-block']}>
-          <Image.PreviewGroup
-            items={productStore.product?.gallery?.map((img) => `data:image/png;base64,${img}`)}
-            preview={{
-              toolbarRender: (_, { transform: { scale }, actions: { onZoomOut, onZoomIn } }) => (
-                <Space size={24} className="toolbar-wrapper">
-                  <ZoomOutOutlined style={{ fontSize: '1.25rem' }} disabled={scale === 1} onClick={onZoomOut} />
-                  <ZoomInOutlined style={{ fontSize: '1.25rem' }} disabled={scale === 50} onClick={onZoomIn} />
-                </Space>
-              ),
-            }}
-          >
-            <Image src={`data:image/png;base64,${productStore.product?.gallery![0]}`} />
-          </Image.PreviewGroup>
-        </div>
-        <div className={styles['info-block']}>
-          <div className={styles['header-block']}>
-            <Title level={2}>{productStore.product?.title}</Title>
-          </div>
-          <div className={styles['rate-block']}>
-            <Rate allowHalf disabled value={productStore.product?.rating} />
-            <Paragraph copyable>{`#${productStore.product?.vendorCode}`}</Paragraph>
-          </div>
-          <div className={styles['price-block']}>
-            <Text
-              className={productStore.product?.discountedPrice ? styles['old-price'] : ''}
-            >{`$${productStore.product?.price}`}</Text>
-            {!!productStore.product?.discountedPrice && (
-              <Text className={styles['discounted-price']}>{`$${productStore.product.discountedPrice}`}</Text>
-            )}
-          </div>
-          <Paragraph ellipsis={{ rows: 5, expandable: 'collapsible' }}>{productStore.product?.description}</Paragraph>
-          <div className={styles['size-block']}>
-            <Paragraph>Size:</Paragraph>
-            <Radio.Group
-              onChange={(e) => {
-                setSize(e.target.value);
-                setQuantity(1);
+    <>
+      <div className={styles.container}>
+        <div className={styles['product-container']}>
+          <div className={styles['image-block']}>
+            <Image.PreviewGroup
+              items={productStore.product?.gallery?.map((img) => `data:image/png;base64,${img}`)}
+              preview={{
+                toolbarRender: (_, { transform: { scale }, actions: { onZoomOut, onZoomIn } }) => (
+                  <Space size={24} className="toolbar-wrapper">
+                    <ZoomOutOutlined style={{ fontSize: '1.25rem' }} disabled={scale === 1} onClick={onZoomOut} />
+                    <ZoomInOutlined style={{ fontSize: '1.25rem' }} disabled={scale === 50} onClick={onZoomIn} />
+                  </Space>
+                ),
               }}
-              value={size}
             >
-              <Radio.Button value="S">S</Radio.Button>
-              <Radio.Button value="M">M</Radio.Button>
-              <Radio.Button value="L">L</Radio.Button>
-            </Radio.Group>
+              <Image src={`data:image/png;base64,${productStore.product?.gallery![0]}`} />
+            </Image.PreviewGroup>
           </div>
-          <div className={styles['cart-block']}>
-            <InputNumber
-              min={1}
-              value={cartItem ? cartItem.quantity : quantity}
-              onChange={(value) => {
-                if (cartItem || !value) return;
-                setQuantity(value);
-              }}
-              disabled={Boolean(cartItem)}
-            />
-            {!cartItem && (
-              <CustomButton variety="common" onClick={handleAddItem}>
-                ADD TO CART
-              </CustomButton>
-            )}
-            {cartItem && (
-              <CustomButton style={{ width: '126px' }} variety="common" onClick={handleRemoveItem}>
-                REMOVE
-              </CustomButton>
-            )}
+          <div className={styles['info-block']}>
+            <div className={styles['header-block']}>
+              <Title level={2}>{productStore.product?.title}</Title>
+            </div>
+            <div className={styles['rate-block']}>
+              <Rate allowHalf disabled value={productStore.product?.rating} />
+              <Paragraph copyable>{`#${productStore.product?.vendorCode}`}</Paragraph>
+            </div>
+            <div className={styles['price-block']}>
+              <Text
+                className={productStore.product?.discountedPrice ? styles['old-price'] : ''}
+              >{`$${productStore.product?.price}`}</Text>
+              {!!productStore.product?.discountedPrice && (
+                <Text className={styles['discounted-price']}>{`$${productStore.product.discountedPrice}`}</Text>
+              )}
+            </div>
+            <Paragraph ellipsis={{ rows: 5, expandable: 'collapsible' }}>{productStore.product?.description}</Paragraph>
+            <div className={styles['size-block']}>
+              <Paragraph>Size:</Paragraph>
+              <Radio.Group
+                onChange={(e) => {
+                  setSize(e.target.value);
+                  setQuantity(1);
+                }}
+                value={size}
+              >
+                <Radio.Button value="S">S</Radio.Button>
+                <Radio.Button value="M">M</Radio.Button>
+                <Radio.Button value="L">L</Radio.Button>
+              </Radio.Group>
+            </div>
+            <div className={styles['cart-block']}>
+              <InputNumber
+                min={1}
+                value={cartItem ? cartItem.quantity : quantity}
+                onChange={(value) => {
+                  if (cartItem || !value) return;
+                  setQuantity(value);
+                }}
+                disabled={Boolean(cartItem)}
+              />
+              {!cartItem && (
+                <CustomButton variety="common" onClick={handleAddItem}>
+                  ADD TO CART
+                </CustomButton>
+              )}
+              {cartItem && (
+                <CustomButton style={{ width: '126px' }} variety="common" onClick={handleRemoveItem}>
+                  REMOVE
+                </CustomButton>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <Title level={2}>PRODUCT DETAILS</Title>
-        <Divider style={{ marginTop: 0 }} />
-        <div className={styles['product-details-container']}>
-          <div>
-            <Paragraph>{productStore.product?.description}</Paragraph>
-          </div>
-          <div className={styles['overview-block']}>
+        <div>
+          <Title level={2}>PRODUCT DETAILS</Title>
+          <Divider style={{ marginTop: 0 }} />
+          <div className={styles['product-details-container']}>
             <div>
-              <Title level={5}>PRODUCT FEATURES</Title>
-              <Paragraph>
-                <ul>{productStore.product?.overview.map((item, index) => <li key={`${index}`}>{item}</li>)}</ul>
-              </Paragraph>
+              <Paragraph>{productStore.product?.description}</Paragraph>
             </div>
-            <div>
-              <Title level={5}>3 YEARS WARRANTY</Title>
-              <div className={styles['warranty-block']}>
-                <div className={styles.logo}>
-                  <LogoIcon />
+            <div className={styles['overview-block']}>
+              <div>
+                <Title level={5}>PRODUCT FEATURES</Title>
+                <Paragraph>
+                  <ul>{productStore.product?.overview.map((item, index) => <li key={`${index}`}>{item}</li>)}</ul>
+                </Paragraph>
+              </div>
+              <div>
+                <Title level={5}>3 YEARS WARRANTY</Title>
+                <div className={styles['warranty-block']}>
+                  <div className={styles.logo}>
+                    <LogoIcon />
+                  </div>
+                  <Paragraph>{WARRANTY_TEXT}</Paragraph>
                 </div>
-                <Paragraph>{WARRANTY_TEXT}</Paragraph>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      {productStore.product?.category === 'bikes' && (
-        <>
-          <TechSpecs />
-          <Geometry />
-        </>
-      )}
-      <div>
-        <Title level={2}>YOU MIGHT LIKE</Title>
-        <Divider style={{ marginTop: 0 }} />
-        <div className={classNames(styles['product-details-container'])}>
-          <BestBikes />
+        {productStore.product?.category === 'bikes' && (
+          <>
+            <TechSpecs />
+            <Geometry />
+          </>
+        )}
+        <div>
+          <Title level={2}>YOU MIGHT LIKE</Title>
+          <Divider style={{ marginTop: 0 }} />
+          <div className={classNames(styles['product-details-container'])}>
+            <BestBikes />
+          </div>
         </div>
       </div>
-    </div>
+      {contextHolder}
+    </>
   );
 }
 
