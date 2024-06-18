406f7d0274364a353ec01f2d4595a4739b610f8a
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-06-18 18:47:38 +0300

    feat: delete message component from the promo text

--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unsafe-optional-chaining */
-import { Card, message, Typography } from 'antd';
+import { Card, Typography } from 'antd';
 import { observer } from 'mobx-react-lite';
 import { cartStore } from '../../store/cart-store';
 
@@ -15,24 +15,13 @@ interface Props {
 }
 
 export default observer(function MainBanner({ slogan, promoCode }: Props) {
-  const onApplyPromoCode = () => {
-    navigator.clipboard
-      .writeText(promoCode)
-      .then(() => {
-        message.success('Promo code copied to clipboard!');
-      })
-      .catch(() => {
-        message.error('Failed to copy promo code.');
-      });
-  };
-
   return (
     <section className={styles.banner} style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Card bordered={false} className={styles.bannerCard}>
         <h2 className={styles.bannerSlogan}>{slogan}</h2>
         <Text className={styles.bannerPromo}>
           Use the code{' '}
-          <Text strong copyable={{ text: promoCode }} onClick={onApplyPromoCode}>
+          <Text strong copyable={{ text: promoCode }}>
             {promoCode}
           </Text>{' '}
           and get a {cartStore.promoCodes[1]?.discount * 100}% discount for the first order!
