a734ed22b2730218f9e2e01eefe89b6be501ae48
Hrybach Oleksiy <84387553+hrybach-oleksiy@users.noreply.github.com>
2024-06-18 06:15:28 -0500

    feat: RSS-ECOMM-4_17: Display Original and Discounted Prices After Promo Code Application (#65)
    * feat: add promo section
    
    * feat: add redirect to the site sections
    
    * style: add adaptive to the promo cards
    
    * fix: change promo amount, fix technology in footer
    
    * feat: synchronize front end and back end parts of the task
    
    * style: update promo and banner styles, add css variables
    
    * chore: skip test to check the deploy

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,13 @@
 import Hero from '../../components/Hero/Hero';
 import BestBikes from '../../components/BestBikes/BestBikes';
+import Promo from '../../components/Promo/Promo';
 
 function MainPage() {
   return (
     <>
       <Hero style={{ maxWidth: '100vw' }} />
       <div className="container">
+        <Promo />
         <BestBikes />
       </div>
     </>
